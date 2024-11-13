3e14e9a9b7ca558b6e1830a24b45273f1efc6aba
Osmanovvv <osmanov.ser.i.23@gmail.com>
2024-11-13 21:50:31 +0300

    4.3

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Сервер разрабатывает игру */
+/* Сервер разрабатывает */
 router.get('/development', function(req, res, next) { 
-  res.send("<h1>Страница Сервера 1</h1>")
-});
-
+  res.render('server', {
+    title: "Сервер работает",
+    picture: "images/development.jpg",
+    desc: "Привет, я Сервер, frontend разработчик. Создаю удобные и функциональные интерфейсы, чтобы сделать взаимодействие с приложениями простым и увлекательным."
+   
+  });
+  });
+  
 /* Сервер с программистами */
 router.get('/programmers', function(req, res, next) { 
-  res.send("<h1>Страница Сервера 2/h1>")
-});
+  res.render('server', {
+    title: "Сервер с программистами",
+    picture: "images/programmers.jpg",
+    desc: "Привет, я Сервер, frontend разработчик. На этом фото я в компании опытных программистов, с которыми делюсь идеями и вдохновляюсь для новых решений в разработке."
+   
+  });
+  });
 
 /* Сервер в будущем */
 router.get('/future', function(req, res, next) { 
-  res.send("<h1>Страница Сервера 3</h1>")
-});
-
-
+  res.render('server', {
+    title: "Сервер в будущем",
+    picture: "images/future.jpg",
+    desc: "Привет, я Сервер, frontend разработчик. На этом фото я — уже состоявшийся человек, который прошел долгий путь и добился успеха, претворяя свои идеи в жизнь"
+   
+  });
+  });
 
 
 module.exports = router;
