0f5552936be1a734e8ca3c22e4a9697e6496e6f3
Osmanovvv <osmanov.ser.i.23@gmail.com>
2024-11-13 20:42:56 +0300

    4.2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Сервер разрабатывает игру */
+router.get('/development', function(req, res, next) { 
+  res.send("<h1>Страница Сервера 1</h1>")
+});
+
+/* Сервер с программистами */
+router.get('/programmers', function(req, res, next) { 
+  res.send("<h1>Страница Сервера 2/h1>")
+});
+
+/* Сервер в будущем */
+router.get('/future', function(req, res, next) { 
+  res.send("<h1>Страница Сервера 3</h1>")
+});
+
+
+
+
 module.exports = router;
