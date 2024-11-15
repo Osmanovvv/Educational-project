16af46b3ac73d61007b3a525d701aea2812687d1
Osmanovvv <osmanov.ser.i.23@gmail.com>
2024-11-15 19:02:01 +0300

    7.2

--- testMongoose.js
+++ testMongoose.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024'); 
 
-const Server = mongoose.model('Server', { name: String });
+// Подключение к базе данных
+mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
-const server = new Server({ name: 'Сервер' }); 
-server.save().then(() => console.log('Османов Сервер!'));
+// Создание схемы
+var schema = mongoose.Schema({ name: String });
+
+// Добавление метода в схему
+schema.methods.sayServer = function () {
+  console.log(this.name + " говорит, что он работает!");
+};
+
+// Создание модели на основе схемы
+const Server = mongoose.model('Server', schema);
+
+// Создание нового документа и сохранение его в базе данных
+const server = new Server({ name: 'Сервер' });
+server.save().then(() => server.sayServer());
