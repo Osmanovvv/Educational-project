d041612cfbed98a5bbebaf51304060a370b139a2
Osmanovvv <server.osmanov26@mail.ru>
2024-11-20 19:27:05 +0300

    7.3

--- testMongoose.js
+++ testMongoose.js
@@ -3,17 +3,12 @@ const mongoose = require('mongoose');
 // Подключение к базе данных
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
-// Создание схемы
-var schema = mongoose.Schema({ name: String });
+var Server = require('./models/server.js').Server
 
-// Добавление метода в схему
-schema.methods.sayServer = function () {
-  console.log(this.name + " говорит, что он работает!");
-};
+var server = new Server({
+   title: "Сервер работает",
+   nick: "WorkingServer",
+  })
 
-// Создание модели на основе схемы
-const Server = mongoose.model('Server', schema);
 
-// Создание нового документа и сохранение его в базе данных
-const server = new Server({ name: 'Сервер' });
-server.save().then(() => server.sayServer());
+server.save();
